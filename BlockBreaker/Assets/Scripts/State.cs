e172b67fdb09e92cfa4db1e8bec2b2bebc9dd6d0
David Cutts <davidcutts@Davids-MBP.attlocal.net>
2020-04-05 15:17:04 -0700

    Adding repo

---
+++ BlockBreaker/Assets/Scripts/State.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject{
+
+    [TextArea(14,10)] [SerializeField] string questionText;
+    [SerializeField] State[] nextStates;
+
+    public string GetStateStory()
+    {
+        return questionText;
+    }
+      public State[] GetNextStates()
+    {
+        return nextStates ;
+    }
+}
